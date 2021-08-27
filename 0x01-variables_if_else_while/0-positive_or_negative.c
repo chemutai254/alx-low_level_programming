3122185b049c6a2449a115f2f729fbd125279580
chemutai254 <nancy.chemutai254@gmail.com>
2021-08-27 03:51:45 -0700

    the program assigns random numbers during execution

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - determines if the number is positive or negative
+ * Description: program to assign a random number to the variable each time
+ * it is executed.
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+int n;
+
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n == 0)
+printf("%d is zero\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printf("%d is positive\n", n);
+
+return (0);
+}
