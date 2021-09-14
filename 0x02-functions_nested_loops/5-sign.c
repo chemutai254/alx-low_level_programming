595cc990864abb77a5fcda80a48d9af721c3633c
chemutai254 <nancy.chemutai254@gmail.com>
2021-09-14 05:02:38 -0700

    function that prints sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#incude "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *@n: n is a sign
+ *
+ * Return: 1 if n is greater than zero
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (+);
+}
+else if (n < 0)
+{
+return (-);
+}
+else
+{
+return (0);
+}
+}
