d9b1b3da58f1c23bffb267d024ee31a077ca9048
chemutai254 <nancy.chemutai254@gmail.com>
2021-09-14 05:06:32 -0700

    function that prints sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#incude "main.h"
+#include "main.h"
 
 /**
  * print_sign - prints the sign of a number
@@ -11,11 +11,11 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-return (+);
+return ('+');
 }
 else if (n < 0)
 {
-return (-);
+return ('-');
 }
 else
 {
