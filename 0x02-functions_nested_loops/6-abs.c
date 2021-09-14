e401c8878a8808c9157bd816bde920e0e4a6d501
chemutai254 <nancy.chemutai254@gmail.com>
2021-09-14 05:29:41 -0700

    Prints absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ *@int: prints an absolute value
+ * Return: an integer
+ */
+
+int _abs(int)
+{
+int a, num;
+a = abs(num);
+_putchar(a);
+_putchar('\n');
+}
