745e0b7490b088ebb41e4325407225dfd16353cc
chemutai254 <nancy.chemutai254@gmail.com>
2021-09-17 12:07:01 -0700

    function that prints nine times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,10 +12,9 @@ void times_table(void)
 int num, count;
 num = 9;
 
-tables(num);
-for (count = 0; count <= 9; count++)
+for (count = 1; count <= 9; count++)
 {
-_putchar('%d * %d = %d ', num, count, num *count);
+_putchar('%d * %d = %d ', num, count, (num *count));
 }
 _putchar('\n');
 }
