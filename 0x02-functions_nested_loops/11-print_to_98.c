8a2d7b60670e880621992d90d6f804a4cb6c0d0d
chemutai254 <nancy.chemutai254@gmail.com>
2021-09-20 00:36:01 -0700

    function that prints all prime numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,20 +11,10 @@
 
 void print_to_98(int n)
 {
-if (n < 98)
+while (n > 0 && n < 98)
 {
-while (n < 98)
-{
-_putchar('%d ', n);
+_putchar(n);
 n++;
 }
-}
-else if (n < 98)
-{
-while (n < 98)
-{
-_putchar('%d, ', n);
-}
-}
-_putchar('98\n');
+_putchar(\n');
 }
