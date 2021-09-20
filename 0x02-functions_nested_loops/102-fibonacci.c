4f34866bca78fa40ca9a58de8a0acdca7d9db3fe
chemutai254 <nancy.chemutai254@gmail.com>
2021-09-20 02:43:51 -0700

    function that prints fibonacci series

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,15 +3,15 @@
 
 /**
  * main - prints fibonacci series
- *
+ * @a: integer
  * Description: prints first 50 fibonacci numbers
  *
  * Return: 0
  */
 
-int main(void)
+int main(int a)
 {
-int a, b = 1, c;
+int b = 1, c;
 
 for (a = 0 ; a < 50; a++)
 {
@@ -20,6 +20,5 @@ _putchar(c);
 a = b;
 b = c;
 _putchar('\n');
-return (0);
 }
 }
