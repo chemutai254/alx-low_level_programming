8deccc2eff6aa34f202006617dc74d5afb3234c0
chemutai254 <nancy.chemutai254@gmail.com>
2021-09-20 01:12:20 -0700

    function that prints a table n times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,6 +12,8 @@ void print_times_table(int n)
 {
 int row, col, product;
 
+if n >= 0 && n < 15)
+{
 for (row = 0; row <= n; row++)
 {
 for (col = 0; col <= n; col++)
@@ -44,6 +46,7 @@ _putchar((product / 10) % 10 + '0');
 _putchar(product % 10 + '0');
 }
 _putchar('\n');
+}
 }
 }
 }
