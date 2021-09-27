a80ae351ff913b18751e558a01bfb531cd71fcab
chemutai254 <nancy.chemutai254@gmail.com>
2021-09-27 11:06:29 -0700

    prints numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
