d29c2ff4a5392ebb1b1f59e8a9879455efa2f20c
chemutai254 <nancy.chemutai254@gmail.com>
2021-09-27 10:32:33 -0700

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#incude "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character
+ * @c: checked for uppercase
+ * Return: 1 if c is uppercase and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
