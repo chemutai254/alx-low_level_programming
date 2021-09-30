cba05b0478103c2f3cdd31fbe8abdd023a4f3bd1
chemutai254 <nancy.chemutai254@gmail.com>
2021-09-30 05:07:21 -0700

    prints largest prime factor number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - prints prime factors
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int num = 612852475143;
+	int prime;
+
+	for (prime = 2; prime <= sqrt(num); prime++)
+	{
+		if (num % prime == 0)
+		{
+			num = num / prime;
+			prime = 1;
+		}
+	}
+	printf("%ld\n", num);
+	return (0);
+}
