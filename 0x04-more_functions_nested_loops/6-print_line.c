dc0dd9e767bb12b3519be328cb4420b54071098a
chemutai254 <nancy.chemutai254@gmail.com>
2021-09-29 22:49:54 -0700

    prints a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line in the terminal
+ *
+ * @n: parameter integer
+ *
+ * Return: n
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
