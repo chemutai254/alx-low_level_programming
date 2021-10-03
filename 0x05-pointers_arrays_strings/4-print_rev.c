0bb047f4870882adf3249360e38ec24f40c053b7
chemutai254 <nancy.chemutai254@gmail.com>
2021-10-03 01:15:08 -0700

    print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,39 +11,14 @@
 
 void print_rev(char *s)
 {
-	int len, i;
+	int i;
 
-	char *start, *end, temp;
-
-	len = str_len(s);
-	start = s;
-	end = s;
-
-	for (i = 0; i < len / 2; i++)
+	for (i = 0; s[i] != '\0'; i++);
+	while (i)
 	{
-		temp = *end;
-		*end = *start;
-		*start = temp;
-
-		start++;
-		end--;
+		_putchar(s[i - 1]);
+		i--;
 	}
+
+	_putchar('\n');
 }
-
-int main(void)
-{
-	char s[9] = 'Holberton';
-
-	print_rev(s);
-	return (0);
-}
-
-int str_len(char *ptr)
-{
-	int i = 0;
-
-	while (*(ptr + i) != '\0')
-		i++;
-	return (i);
-}
-
