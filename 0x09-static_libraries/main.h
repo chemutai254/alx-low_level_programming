7b7324cee28e369f119a6ffc058c49148ca2d84e
chemutai254 <nancy.chemutai254@gmail.com>
2021-10-09 02:12:52 -0700

    static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,8 +1,9 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
-int -islower(int c);
+int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
@@ -12,12 +13,14 @@ void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
-char *_strcat(char *dest, char *src, int n);
-char *_strcpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char *accept);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+
+#endif /*MAIN_H*/
