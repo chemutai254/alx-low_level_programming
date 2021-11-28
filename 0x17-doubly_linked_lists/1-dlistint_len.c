5d63aa439df4dea0d2c1477f403706cf6a741752
chemutai254 <nancy.chemutai254@gmail.com>
2021-11-28 04:41:06 -0800

    returns number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elements in a linked list
+ *
+ * @h: pointer to a struct
+ *
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int value = 0;
+
+	while (h)
+	{
+		value++;
+		h = h->next;
+	}
+	return (value)
+}
