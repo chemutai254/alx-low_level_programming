bb17994ab8ccebe5305e482a2138860d62dcadc3
chemutai254 <nancy.chemutai254@gmail.com>
2022-04-08 05:19:53 -0700

    searches for a value in an array

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array
+ * @array: pointer to the first element
+ * @size: the number of elements in an array
+ * @value: value to search for
+ *
+ * Return: first index of value or -1 if the array does not exist
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Checked value[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,16 @@
+### 0x1E. C - Search Algorithms
+-- Writing files containing big O notations using formats such as:
+-- O(1)
+-- O(n)
+-- O(n!)
+-- n*m -> O(nm)
+-- n square -> O(n^2)
+-- sqrt n -> O(sqrt(n))
+-- log(n) -> O(log(n))
+-- n * log(n) -> O(nlog(n))
+
+### Tasks
+-- Linear search
+-- Binary search
+-- Big O #0 - Time complexity
+-- Big O #1 - Space complexity
